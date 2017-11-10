4ce412ce7022021f7a3104dea5e11e244f95ea4e
Björn Fröhling <bjorn.frohling@qvik.fi>
2017-11-10 06:57:04 +0200

    Added function for storing device token. Added function for sending push notification

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -1,25 +1,37 @@
 import Vapor
+import VaporAPNS
 
 extension Droplet {
-    func setupRoutes() throws {
-        get("hello") { req in
-            var json = JSON()
-            try json.set("hello", "world")
-            return json
-        }
-
-        get("plaintext") { req in
-            return "Hello, world!"
-        }
-
-        // response to requests to /info domain
-        // with a description of the request
-        get("info") { req in
-            return req.description
-        }
-
-        get("description") { req in return req.description }
-        
-        try resource("posts", PostController.self)
+  func setupRoutes() throws {
+    
+    // Function for storing mobile device token
+    post("registerPush") { request in
+      let mc = try MobileClient(request: request)
+      try mc.save()
+      
+      return try mc.makeJSON()
     }
+    
+    // Function for sending push notifications to all stored devices
+    get("push") { _ in
+      let apps = try MobileClient.all()
+      for mc in apps {
+        let notification = try Notification(mobileClient: mc, message: "A push message")
+        try notification.send()
+      }
+      
+      return "push sent"
+    }
+    
+    // response to requests to /info domain
+    // with a description of the request
+    get("info") { req in
+      return req.description
+    }
+    
+    get("description") { req in return req.description }
+    
+    try resource("posts", PostController.self)
+  }
 }
+
