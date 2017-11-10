4085a10ca04404f7c672b8b88827686c395ab6f0
Björn Fröhling <bjorn.frohling@qvik.fi>
2017-11-10 06:33:01 +0200

    Added VaporAPNS module

--- Package.swift
+++ Package.swift
@@ -3,24 +3,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "TrendNotification",
-    products: [
-        .library(name: "App", targets: ["App"]),
-        .executable(name: "Run", targets: ["Run"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
-        .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
-    ],
-    targets: [
-        .target(name: "App", dependencies: ["Vapor", "FluentProvider"],
-                exclude: [
-                    "Config",
-                    "Public",
-                    "Resources",
-                ]),
-        .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
-    ]
+  name: "TrendPushNotification",
+  products: [
+    .library(name: "App", targets: ["App"]),
+    .executable(name: "Run", targets: ["Run"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
+    .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
+    .package(url: "https://github.com/matthijs2704/vapor-apns.git", .upToNextMajor(from: "2.1.0"))
+  ],
+  targets: [
+    .target(name: "App", dependencies: ["Vapor", "FluentProvider", "VaporAPNS"],
+            exclude: [
+              "Config",
+              "Public",
+              "Resources",
+              ]),
+    .target(name: "Run", dependencies: ["App"]),
+    .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
+  ]
 )
-
